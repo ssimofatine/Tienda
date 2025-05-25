07c7e7498998ef51b7d057db0ee79ac269e034bf
ssimofatine <simohaji147@gmail.com>
2025-05-25 22:25:39 +0200

    extraido metodo ventaPorPantalla

--- src/Tienda.java
+++ src/Tienda.java
@@ -36,10 +36,7 @@ public class Tienda {
 	                // Reducimos el stock del producto
 	                producto.reducirStock(cantidadVendida);    
 	                // Mostramos venta por pantalla
-	                System.out.println("Venta "+ producto.getNombre()+" realizada. ");
-	                System.out.println("Subtotal : " + totalVenta);
-	                System.out.println("Decuento aplicado: "+descuento);
-	                System.out.println("Total : " + totalConDescuento);
+	                ventaPorPantalla(producto, totalVenta, descuento, totalConDescuento);
 	            } else {
 	                System.out.println("Stock insuficiente.");
 	            }
@@ -51,6 +48,13 @@ public class Tienda {
 	    if (!productoEncontrado) {
 	        System.out.println("Producto "+nombreProducto +" no encontrado.");
 	    }
+	}
+
+	private void ventaPorPantalla(Producto producto, double totalVenta, double descuento, double totalConDescuento) {
+		System.out.println("Venta "+ producto.getNombre()+" realizada. ");
+		System.out.println("Subtotal : " + totalVenta);
+		System.out.println("Decuento aplicado: "+descuento);
+		System.out.println("Total : " + totalConDescuento);
 	}
 
 	private double calculoDescuentoVenta(double totalVenta) {
